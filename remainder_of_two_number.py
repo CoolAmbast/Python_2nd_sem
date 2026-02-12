47390fb6d4f59d8f8359914872ecece9dcf3dc3a
CoolAmbast <83899058+CoolAmbast@users.noreply.github.com>
2026-02-12 10:37:41 +0530

    added first file

---
+++ remainder_of_two_number.py
@@ -0,0 +1,11 @@
+my_int_1 = 56
+my_int_2 = 12
+
+my_float_1 = 5.4
+my_float_2 = 12.0
+
+mod_ints = my_int_1 % my_int_2
+mod_floats = my_float_2 % my_float_1
+
+print('Integer Modulus:', mod_ints) # Integer Modulus: 8
+print('Float Modulus:', mod_floats) # Float Modulus: 1.1999999999999993
