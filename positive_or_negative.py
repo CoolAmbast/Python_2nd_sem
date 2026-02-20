97e1e9063088d98ee69f6a2046757b7e606cc3af
CoolAmbast <83899058+CoolAmbast@users.noreply.github.com>
2026-02-20 11:19:28 +0530

    modified

--- positive_or_negative.py
+++ positive_or_negative.py
@@ -3,3 +3,7 @@ if num > 0:
     print("The number is positive.")
 elif num < 0:
     print("The number is negative.")
+elif num == 0:
+    print("The number is zero aka neutral.")
+else:
+    print("Tumse na ho payega.")
