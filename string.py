0edc945199400199ed9263bd04ebbf2c0280d23f
CoolAmbast <83899058+CoolAmbast@users.noreply.github.com>
2026-02-23 20:05:08 +0530

    strings_v1

--- string.py
+++ string.py
@@ -7,3 +7,23 @@ deleting strings
 operations on strings
 string functions
 """
+single= 'This is a single quote string'
+double= "This is a double quote string"
+triple= '''This is a triple quote string'''
+s= "Hello Class"
+print(single)
+print(double)
+print(triple)
+#accessing strings
+print(single[0:10])
+print(double[-5])
+print(triple[10:-1])
+print(s[0:5])
+name = "Pranjal"
+#positive indexing
+print(name[0], name[6])
+#negative indexing
+print(name[-7])
+#slicing
+print(s[5:5])
+print(s[6:])
